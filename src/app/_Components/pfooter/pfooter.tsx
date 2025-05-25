f68cdac9a8e4f4d5c6b8c44b0151c80f0006a581
First-React-project <hbasuone@gamil.com>
2025-05-25 23:42:10 +0300

    update

--- src/app/_Components/pfooter/pfooter.tsx
+++ src/app/_Components/pfooter/pfooter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Pfooter() {
   return (
     <div>
-        <footer className="bg-gray-200 text-center lg:text-left">
+        <footer className="bg-red-400 text-center lg:text-left">
             <div className="text-gray-600 text-center p-4">
             © 2023 Healhub. All rights reserved.
             </div>
